1c5d3200fce63d4648b13533cba0bb286969b231
Merijn <merijn@ponzo.nl>
2023-07-10 15:16:58 +0200

    all

--- build/group/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '01d7c1d618c5b49dbdf2');
